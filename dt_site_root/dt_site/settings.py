60ef002688a36b879417657c9eb1f5deaa279131
F1asher <cheskidovoleg@gmail.com>
2022-07-03 18:38:14 +0700

    In settings.py add tz, templates.dirs add "BASEDIR / 'dt_site/templates'" and create this folder; add STATICFILES_DIRS = [BASE_DIR / 'dt_site/static'] and create it. add to templates base.html, add to static main.css, edit view with render. add block content to base.html; create dir templates/pages in pages.

--- dt_site_root/dt_site/settings.py
+++ dt_site_root/dt_site/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'dt_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'dt_site/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Novosibirsk'
 
 USE_I18N = True
 
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'dt_site/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- dt_site_root/pages/urls.py
+++ dt_site_root/pages/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # path('', views.index, {'pagename': ''}, name='home'),
+    # path('<str:pagename>', views.index, name='index'),
 ]
