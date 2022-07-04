55bb581ff575145d607d56d377f53ded34c3042e
F1asher <cheskidovoleg@gmail.com>
2022-07-04 12:17:26 +0700

    create testform.html. create forms.py in pages dir. add URLpattern to pages.urls for form and add this form to base.html. create pages/templates file contact.html. create contact form view. edit main.css for contact form. add console_email_sender to views.py.

--- dt_site_root/pages/views.py
+++ dt_site_root/pages/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.core.mail import get_connection, send_mail
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+
+from .forms import ContactForm
 from .models import Page
 
 
 def index(request, pagename):
     pagename = '/' + pagename
-    pg = Page.objects.get(permalink=pagename)
+    pg = get_object_or_404(Page, permalink=pagename)
     context = {
         'title': pg.title,
         'content': pg.bodytext,
@@ -13,3 +17,27 @@ def index(request, pagename):
     }
     # assert False
     return render(request, 'pages/page.html', context)
+
+
+def contact(request):
+    submitted = False
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            con = get_connection('django.core.mail.backends.console.EmailBackend')
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),
+                ['siteowner@email.com'],
+                connection=con,
+            )
+            # assert False
+            return HttpResponseRedirect('/contact?submitted=True')
+    else:
+        form = ContactForm()
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request, 'pages/contact.html', {'form': form, 'page_list': Page.objects.all(), 'submitted': submitted})
