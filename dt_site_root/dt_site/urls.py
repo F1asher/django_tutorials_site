5759d4c70844b0879650c3930fe47fa4f9d68532
F1asher <cheskidovoleg@gmail.com>
2022-07-06 13:58:33 +0700

    create author group in admin panel of site. create testuser and add it to group. create class Register. create templates: login, register, success. edit base.html for display user auth. add URLs to base URL file. edit quote/views: quote_req add @login_required(login_url=(reverse_lazy('login'))) and modify func; QuoteList, QuoteView modify classes with Mixin. add error_hanlers  to base URLs and templates/bag_request.html

--- dt_site_root/dt_site/urls.py
+++ dt_site_root/dt_site/urls.py
@@ -14,13 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
 from django.views.generic.base import TemplateView
+from django.urls import include, path
+from quotes.views import Register
+
+
+handler400 = handler403 = handler404 = handler500 = 'quotes.views.bad_request'
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('testpage', TemplateView.as_view(template_name='pages/page.html')),
+    # path('testpage', TemplateView.as_view(template_name='pages/page.html')),
+    path('register/success', TemplateView.as_view(template_name='registration/success.html'), name='register-success'),
+    path('register/', Register.as_view(), name='register'),
     path('quote/', include('quotes.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
 ]
